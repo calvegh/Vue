3f0935240d726958464b1668bfbe5ffc7b6ca384
Carlos Vega <calvegh@gmail.com>
2022-05-25 10:55:06 -0400

    Avances

---
+++ seccion7/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,42 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ friendIsFavorite ? "(Favorite)" : "" }}</h2>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }} Details
+    </button>
+    <button @click="toggleFavorite">Toggle Favorite</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone: </strong>{{ phoneNumber }}</li>
+      <li><strong>Email: </strong>{{ emailAdress }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friendIsFavorite: this.isFavorite,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      this.friendIsFavorite = !this.friendIsFavorite;
+    },
+  },
+  //props: ["name", "phoneNumber", "emailAdress","isFavorite"],
+  props: {
+    name: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    emailAdress: { type: String, required: true },
+    isFavorite: { type: Boolean, required: false, default: false },
+    /* validator: function (value) {
+      return value === "1" || value === "0";
+    }, */
+  },
+};
+</script>
