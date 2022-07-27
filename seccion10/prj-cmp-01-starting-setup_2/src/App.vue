6363d651e1d1ed41bb11782ffd0ba2846e2b2d32
Carlos Vega <calvegh@gmail.com>
2022-07-27 12:39:39 -0400

    seccion10

---
+++ seccion10/prj-cmp-01-starting-setup_2/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+<the-header title="Remember Me"></the-header>
+<stored-resources :resources="storedResources"></stored-resources>
+</template>
+
+<script>
+import storedResources from './components/learning-resources/StoredResources.vue'
+import TheHeader from './components/layouts/TheHeader.vue'
+
+export default {
+  components: {'stored-resources':storedResources,
+  'the-header':TheHeader,
+ },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The oficial Vue.js documentation',
+          link: 'https://vuejs.org/v2/guide/index.html',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google',
+          link: 'https://google.org',
+        },
+      ],
+    };
+  },
+};
+</script>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+    *{
+        box-sizing: border-box;
+    }
+    html{
+        font-family:'Roboto', sans-serif;
+    }
+    body{
+        margin:0
+    }
+</style>
