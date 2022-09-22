72f31cb3b120b82d4d6f65ad040f49f2c9dc8842
Carlos Alberto Vega Hernández <calvegh@gmail.com>
2022-09-22 18:29:55 -0300

    86% listo

--- seccion16/main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
+++ seccion16/main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
@@ -1,24 +1,35 @@
 <template>
   <div>
+    <base-dialog :show="!!error" title="An error ocurred"
+    @close="handleError">
+      <p>
+        {{ error }}
+      </p>
+    </base-dialog>
     <section><coach-filter @change-filter="setFilters"></coach-filter></section>
     <section>
       <base-card>
-      <div class="controls">
-        <base-button mode="outline">Refresh</base-button>
-        <base-button link to="/register">Register as Coach</base-button>
-      </div>
-      <ul v-if="hasCoaches">
-        <coach-item
-          v-for="coach in filteredCoaches"
-          :key="coach.id"
-          :id="coach.id"
-          :first-name="coach.firstName"
-          :last-name ="coach.lastName"
-          :rate ="coach.hourlyRate"
-          :areas ="coach.areas"
-        ></coach-item>
-      </ul>
-      <h3 v-else>No coaches found.</h3>
+        <div class="controls">
+          <base-button mode="outline" @click="loadCoaches">Refresh</base-button>
+          <base-button v-if="!isCoach && !isLoading" link to="/register"
+            >Register as Coach</base-button
+          >
+        </div>
+        <div v-if="isLoading">
+          <base-spinner></base-spinner>
+        </div>
+        <ul v-else-if="hasCoaches">
+          <coach-item
+            v-for="coach in filteredCoaches"
+            :key="coach.id"
+            :id="coach.id"
+            :first-name="coach.firstName"
+            :last-name="coach.lastName"
+            :rate="coach.hourlyRate"
+            :areas="coach.areas"
+          ></coach-item>
+        </ul>
+        <h3 v-else>No coaches found.</h3>
       </base-card>
     </section>
   </div>
@@ -26,57 +37,78 @@
 
 <script>
 import coachItem from '../../components/coaches/CoachItem.vue';
-import CoachFilter from '../../components/coaches/CoachFilter.vue'
+import CoachFilter from '../../components/coaches/CoachFilter.vue';
 
 export default {
   components: {
     coachItem,
-    CoachFilter
+    CoachFilter,
   },
-  data(){
+  data() {
     return {
-      activeFilters:{
-        frontend:true,
-        backend:true,
-        career:true,
-      }
-    }
+      isLoading: false,
+      error: null,
+      activeFilters: {
+        frontend: true,
+        backend: true,
+        career: true,
+      },
+    };
   },
   computed: {
     filteredCoaches() {
       const coaches = this.$store.getters['coaches/coaches'];
-      return coaches.filter(coach=>{
-        if(this.activeFilters.frontend && coach.areas.includes('frontend')){
-          return true
+      return coaches.filter((coach) => {
+        if (this.activeFilters.frontend && coach.areas.includes('frontend')) {
+          return true;
         }
-        if(this.activeFilters.backend && coach.areas.includes('backend')){
-          return true
+        if (this.activeFilters.backend && coach.areas.includes('backend')) {
+          return true;
         }
-        if(this.activeFilters.career && coach.areas.includes('career')){
-          return true
+        if (this.activeFilters.career && coach.areas.includes('career')) {
+          return true;
         }
-        return false
-      })
+        return false;
+      });
     },
     hasCoaches() {
-      return this.$store.getters['coaches/hasCoaches'];
+      return !this.isLoading && this.$store.getters['coaches/hasCoaches'];
+    },
+    isCoach() {
+      return this.$store.getters['coaches/isCoach'];
     },
   },
-  methods:{
-    setFilters(updatedFilters){
-      this.activeFilters = updatedFilters
+  methods: {
+    setFilters(updatedFilters) {
+      this.activeFilters = updatedFilters;
+    },
+    async loadCoaches() {
+      this.isLoading = true;
+      try {
+        await this.$store.dispatch('coaches/loadCoaches');
+      } catch (error) {
+        this.error = error.message || 'Something went wrong!';
+      }
+
+      this.isLoading = false;
+    },
+    handleError(){
+      this.error = null;
     }
-  }
+  },
+  created() {
+    this.loadCoaches();
+  },
 };
 </script>
 
 <style scoped>
-ul{
+ul {
   list-style: none;
-  margin:0;
-  padding:0;
+  margin: 0;
+  padding: 0;
 }
-.controls{
+.controls {
   display: flex;
   justify-content: space-between;
 }
